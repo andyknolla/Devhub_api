42068012d7d35273e1be2c71ba3a4183d595955d
Andy Knolla <andyknolla@gmail.com>
2017-06-01 17:16:53 -0600

    Create, implement/test seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,80 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |tag|
+  Tag.create!(
+    name: "Tag #{tag}"
+  )
+end
+
+3.times do |category|
+  Category.create!(
+    name: "Javascript? #{category}"
+  )
+end
+
+3.times do |type|
+  ResourceType.create!(
+    name: "Type #{type}"
+  )
+end
+
+3.times do |user|
+  User.create!(
+    username: "User#{user}"
+  )
+end
+
+4.times do |resource|
+  Resource.create!(
+    title: "Title #{resource}",
+    url: "Url #{resource}",
+    rating: 5,
+    description: "Crash pad cold shut bi-pattern chains a tagline wall crack limestone aid.  Corner spire tips crux rope drag sequence and scary knife-blade slot creek.  Auto-block biner crystal anchor descent and equalette.",
+    free: true,
+    category_id: Category.last.id,
+    resource_type_id: ResourceType.last.id
+  )
+end
+4.times do |resource|
+  Resource.create!(
+    title: "Title #{resource}",
+    url: "Url #{resource}",
+    rating: 3,
+    description: "Headwalls and nut-tools on belay dogbone sloper chalk, fixed piece thumbcatch gobie jug.  Chickenwing bootie hex chimney, classic RP portaledge.  Kingswing floss rock and tricam pocket trigger boulder.",
+    free: false,
+    category_id: Category.first.id,
+    resource_type_id: ResourceType.first.id
+  )
+end
+
+3.times do |comment|
+  Comment.create!(
+    body:"Spooky poop tube a crimp ladder daisy chain, etrier tower placement and hueco gri-gri chickenhead sling.  Lead piton rurp on klemheist layback bail.  Wagbag is mantle knot v-thread monstercam gear for verglase dome sandwiches.",
+    user_id: User.first.id,
+    resource_id: Resource.first.id
+  )
+end
+
+3.times do |comment|
+  Comment.create!(
+    body:"Oldschool tape glove crush and slabby topo granite is coreshot redpoint.  Joshua Tree 5.9, manky offwidth guidebook and gnarly jumar clipping stance, pro tipped out sketchy bombproof loose block rap ring.",
+    user_id: User.last.id,
+    resource_id: Resource.last.id
+  )
+end
+
+Favorite.create!(
+  user_id: User.last.id,
+  resource_id: Resource.last.id
+)
+Favorite.create!(
+  user_id: User.first.id,
+  resource_id: Resource.first.id
+)
+
+TagResource.create!(
+  resource_id: Resource.last.id,
+  tag_id: Tag.last.id
+)
+TagResource.create!(
+  resource_id: Resource.first.id,
+  tag_id: Tag.first.id
+)
